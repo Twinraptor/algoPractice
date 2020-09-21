816e66f209d5e22daaab578600e17adb8f4051a3
twinraptor <mythosx@hotmail.com>
2020-09-20 21:59:52 -0700

    added a few new algos

--- Algos.js
+++ Algos.js
@@ -1,2 +1,23 @@
 //Body of algo problems
 console.log("hello world");
+
+//Sum of two numbers
+let sumOfTwoNumbers = (a, b) => a + b;
+
+console.log(sumOfTwoNumbers(7, 9));
+
+//Minutes into seconds
+let minIntoSeconds = (a) => a * 60;
+
+console.log(minIntoSeconds(5));
+
+//Age in seconds function
+
+let yourAgeInSeconds = (age) => age * 365 * 24 * 60 * 60;
+
+console.log(yourAgeInSeconds(41));
+
+//Return first item in array
+let returnFirst = (array) => array[0];
+
+console.log(returnFirst(["Banana", "apples", "carrots"]));
